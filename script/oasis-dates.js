ffe109da251ce02cfe12da7aaf213447fb1ef96f
liwi <77789571+dliwilb@users.noreply.github.com>
2022-02-07 04:32:24 +0100

    done

--- script/oasis-dates.js
+++ script/oasis-dates.js
@@ -20,7 +20,7 @@ async function shakeIt() {
         const marketContractAddress = '0x3b968177551a2aD9fc3eA06F2F41d88b22a081F7';   // Oasis New
         const marketContract = new ethers.Contract(marketContractAddress, NftExV2Abi, provider);
 
-        const nftContractAddress = '0x48973dbAC0d46B939CD12A7250eFBA965e8a8cf2';  // Reapers
+        const nftContractAddress = '0x87aA4eF35454fEF0B3E796d20Ab609d3c941F46b';  // Smart Waifu
 
         let events;
         let contractEvent = '';
@@ -31,7 +31,7 @@ async function shakeIt() {
             const eventFilter = marketContract.filters.MakeOrder(nftContractAddress);
             events = await marketContract.queryFilter(eventFilter, fromBlock, toBlock);
 
-            document.getElementById('list-of-reapers').innerHTML = `<p>[MakeOrder events from block ${fromBlock} to block ${toBlock}]</p><br>`;
+            document.getElementById('list-of-waifus').innerHTML = `<p>[MakeOrder events from block ${fromBlock} to block ${toBlock}]</p><br>`;
             // for (let i = 0; i < events.length; i++) {
             for (let i = events.length-1; i >= 0; i--) {
                 const orderHash = events[i].args[2];
@@ -49,10 +49,14 @@ async function shakeIt() {
                 const nftURI = await nftContract.tokenURI(tokenId);
                 const nftJSON = await fetchJSON(nftURI);
         
-                // document.getElementById('price-of-reaper').innerHTML = nftCurrentPrice;
+                let [kjlastname, kjfirstname, lastname, firstname] = nftJSON.name.split(/ /);
+                firstname = firstname.slice(0,-1);
+                lastname = lastname.toUpperCase().slice(1);
+                const paddingSpaces = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+                const padding = paddingSpaces.slice(0, Math.floor((24 - firstname.length - lastname.length) / 2) * 6);
         
-                document.getElementById('list-of-reapers').innerHTML += 
-                    `<span class="nftdisplay"><span class="nfttext">Reapers #${tokenId} listed for ${nftCurrentPriceBCH} BCH [${auctionType[orderInfo[0]]}]</span><a href="${nftJSON.image}" target="_blank"><img src="${nftJSON.image}" width=280 height=280></a><span class="nfttext">check it out on <a href="https://oasis.cash/token/${nftContractAddress}/${tokenId}" target="_blank">OASIS&thinsp;<img src="icons8-external-link-16_goldish.png"></a></span></span>`;
+                document.getElementById('list-of-waifus').innerHTML += 
+                    `<span class="nftdisplay"><span class="nfttext">Smart Waifu #${tokenId} listed for ${nftCurrentPriceBCH} BCH [${auctionType[orderInfo[0]]}]</span><span class="waifu-name-kj">${kjlastname}&nbsp;&nbsp;${kjfirstname}</span><a href="${nftJSON.image}" target="_blank"><img src="${nftJSON.image}" width=280 height=280></a><span class="waifu-name">${padding}${firstname} ${lastname}${padding} ${nftJSON.attributes[0]["value"]}&nbsp;&nbsp;${nftJSON.attributes[1]["value"]}&nbsp;&nbsp;${nftJSON.attributes[2]["value"]}</span><span class="nfttext">check it out on <a href="https://oasis.cash/token/${nftContractAddress}/${tokenId}" target="_blank">OASIS&thinsp;<img src="icons8-external-link-16_goldish.png"></a></span></span>`;
                     // '<p>Token ID: ' + tokenId + '<br> Seller: ' + seller + '<br> Order Hash: ' + orderHash + '<br>' +
                     // `<img src="${nftJSON.image}" width=300 height=300><br> Current Price: ` + 
                     // nftCurrentPriceBCH + ` BCH @ ${auctionType[orderInfo[0]]} | sold? ${orderInfo[10]} | cancelled? ${orderInfo[11]} | ` + 
@@ -67,7 +71,7 @@ async function shakeIt() {
             events = await marketContract.queryFilter(eventFilter, fromBlock, toBlock);
             // console.log(events);
 
-            document.getElementById('list-of-reapers').innerHTML = `<p>[Claim events from block ${fromBlock} to block ${toBlock}]</p><br>`;
+            document.getElementById('list-of-waifus').innerHTML = `<p>[Claim events from block ${fromBlock} to block ${toBlock}]</p><br>`;
             // for (let i = 0; i < events.length; i++) {
             for (let i = events.length-1; i >= 0; i--) {
                 // event Claim(IERC721 indexed token, uint256 id, bytes32 indexed hash, address seller, address indexed taker, uint256 price);
@@ -81,8 +85,14 @@ async function shakeIt() {
                 const nftURI = await nftContract.tokenURI(tokenId);
                 const nftJSON = await fetchJSON(nftURI);
 
-                document.getElementById('list-of-reapers').innerHTML += 
-                    `<span class="nftdisplay"><span class="nfttext">Reapers #${tokenId} sold for ${nftSellPriceBCH} BCH</span><a href="${nftJSON.image}" target="_blank"><img src="${nftJSON.image}" width=280 height=280></a></span>`;
+                let [kjlastname, kjfirstname, lastname, firstname] = nftJSON.name.split(/ /);
+                firstname = firstname.slice(0,-1);
+                lastname = lastname.toUpperCase().slice(1);
+                const paddingSpaces = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+                const padding = paddingSpaces.slice(0, Math.floor((24 - firstname.length - lastname.length) / 2) * 6);
+
+                document.getElementById('list-of-waifus').innerHTML += 
+                    `<span class="nftdisplay"><span class="nfttext">Smart Waifu #${tokenId} sold for ${nftSellPriceBCH} BCH</span><span class="waifu-name-kj">${kjlastname}&nbsp;&nbsp;${kjfirstname}</span><a href="${nftJSON.image}" target="_blank"><img src="${nftJSON.image}" width=280 height=280></a><span class="waifu-name">${padding}${firstname} ${lastname}${padding} ${nftJSON.attributes[0]["value"]}&nbsp;&nbsp;${nftJSON.attributes[1]["value"]}&nbsp;&nbsp;${nftJSON.attributes[2]["value"]}</span></span>`;
             }
         }
 
